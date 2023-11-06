0f7d30ba288172f11ae086d879280151f35c6f3f
Jihanra <jihanrahadatulasy@gmail.com>
2023-11-07 00:15:53 +0700

    done

--- resources/views/home/about/index.blade.php
+++ resources/views/home/about/index.blade.php
@@ -1,99 +1,58 @@
 <main>
-    <div class="col-lg-10 mx-auto p-3 py-md-4 text-center">
+    <div class="container p-3 p-md-4 text-center">
         <!-- Logo Perusahaan -->
-        <div class="container">
-            <img src="{{ asset('images/icon.png') }}" alt="Gambar Anda" width="215" height="130" style="margin-right: 20px; margin-left: 20px; margin-bottom: 10px;">
-        </div>
+        <img src="{{ asset('images/icon.png') }}" alt="Gambar Anda" width="215" height="130" class="mx-auto d-block mb-4">
 
         <!-- Sejarah Perusahaan -->
         <h1>PT. Karunia Cipta Qinsani</h1>
-        <p class="fs-5 col-md-8 mx-auto text-center"> Mulai berdiri pada tahun 2015 di kota Bekasi.
-            Kami adalah para Profesional yang kreatif
-            dari latar belakang dan profesi yang
-            berbeda-beda namun bersinergi untuk fokus
-            pada tiga (3) bidang yaitu Training,
-            Coaching and Consulting. </p>
-        <p class="fs-5 col-md-8  mx-auto text-center"> Kami bersinergi untuk menjadi salah satu organisasi
-            terkemuka yang menyediakan solusi dalam pengembangan
-            profesional dan organisasi melalui pelatihan, pembinaan
-            dan konsultasi sesuai dengan nilai-nilai yang kami yakjni
-            yang berorientasi kepada hasil yang singnifikan dan
-            berkesinambungan. </p>
-        <hr class="mx-auto my-5 text-center" style="width: 865px;">
+        <p class="fs-5 col-md-8 mx-auto"> Mulai berdiri pada tahun 2015 di kota Bekasi. Kami adalah para Profesional yang kreatif dari latar belakang dan profesi yang berbeda-beda namun bersinergi untuk fokus pada tiga (3) bidang yaitu Training, Coaching and Consulting. </p>
+        <p class="fs-5 col-md-8 mx-auto"> Kami bersinergi untuk menjadi salah satu organisasi terkemuka yang menyediakan solusi dalam pengembangan profesional dan organisasi melalui pelatihan, pembinaan, dan konsultasi sesuai dengan nilai-nilai yang kami yakni yang berorientasi kepada hasil yang signifikan dan berkesinambungan. </p>
+        <hr class="my-5" style="width: 100%; max-width: 865px;">
 
-
-        <!-- VISI dan Misi -->
+        <!-- VISI dan MISI -->
         <div class="row g-5">
-            <div class="col-md-6 text-center">
+            <div class="col-md-6">
                 <h2>VISION</h2>
-                <p>Menjadi salah satu organisasi termuka yang menyediakan solusi dalam pengembangan profesional suatu organisasi melalui pembinaan, konsultasi dan pelatihan di Indonesia dengan integritas, nilai, dan berorientasi hasil bagi para pemangku kepentingan.
+                <p>Menjadi salah satu organisasi terkemuka yang menyediakan solusi dalam pengembangan profesional suatu organisasi melalui pembinaan, konsultasi, dan pelatihan di Indonesia dengan integritas, nilai, dan berorientasi hasil bagi para pemangku kepentingan.</p>
             </div>
-            <div class="col-md-6 text-center">
+            <div class="col-md-6">
                 <h2>MISION</h2>
-                <p>Menyediakan solusi dalam pengembangan profesional suatu organisasi melalui pembinaan, konsultasi, dan pelatihan di Indonesia, memberdayakan individu dan organisasi di Indonesia melalui berbagai solusi pengembangan profesional yang unggul, menyediakan pembinaan yang mendalam untuk membantu individu dan organisasi mencapai potensi penuh mereka, memberikan konsultasi yang relevan untuk membantu para klien dalam menghadapi tantangan dan mencapai tujuan mereka, menyediakan pelatihan berkualitas tinggi yang memungkinkan pengembangan keterampilan dan pengetahuan yang relevan.
+                <p>Menyediakan solusi dalam pengembangan profesional suatu organisasi melalui pembinaan, konsultasi, dan pelatihan di Indonesia, memberdayakan individu dan organisasi di Indonesia melalui berbagai solusi pengembangan profesional yang unggul, menyediakan pembinaan yang mendalam untuk membantu individu dan organisasi mencapai potensi penuh mereka, memberikan konsultasi yang relevan untuk membantu para klien dalam menghadapi tantangan dan mencapai tujuan mereka, menyediakan pelatihan berkualitas tinggi yang memungkinkan pengembangan keterampilan dan pengetahuan yang relevan.</p>
             </div>
         </div>
-        <hr class="mx-auto my-5 text-center" style="width: 865px;">
-        <h2>VALUE </h2>
-        <div class="container-transparent he" style="height: 30px;"></div>
+        <hr class="my-5" style="width: 100%; max-width: 865px;">
+        <h2>VALUE</h2>
+        <div class="he" style="height: 30px;"></div>
 
         <!-- Value -->
-        <div class="row justify-content-center text-center">
+        <div class="row g-4">
             <!-- Commitment -->
-
-            <div class="col-lg-3">
-                <div class="container">
-                    <img src="{{ asset('images/commitment.png') }}" alt="Gambar Anda" width="150" height="130" style="margin-right: 20px; margin-left: 20px;">
-                </div>
-
-                <div class="transparent-container" style="background-color: rgba(255, 255, 255, 0.5); padding: 10px;"></div>
+            <div class="col-md-6 col-lg-3">
+                <img src="{{ asset('images/commitment.png') }}" alt="Commitment" width="150" height="130" class="mx-auto d-block mb-3">
                 <h2>Commitment</h2>
-                <p>Kami memiliki Commitment tinggi
-                    untuk bekerja secara profesional &
-                    memberikan pelayanan terbaik</p>
-            </div><!-- /.col-lg-3 -->
+                <p>Kami memiliki Commitment tinggi untuk bekerja secara profesional & memberikan pelayanan terbaik.</p>
+            </div>
 
             <!-- Innovation -->
-            <div class="col-lg-3">
-                <div class="container">
-                    <img src="{{ asset('images/innovation.png') }}" alt="Gambar Anda" width="150" height="130" style="margin-right: 20px; margin-left: 20px;">
-                </div>
-
-                <div class="transparent-container" style="background-color: rgba(255, 255, 255, 0.5); padding: 10px;"></div>
+            <div class="col-md-6 col-lg-3">
+                <img src="{{ asset('images/innovation.png') }}" alt="Innovation" width="150" height="130" class="mx-auto d-block mb-3">
                 <h2>Innovation</h2>
-                <p>Kami berorientasi pada Inovasi
-                    untuk meningkatkan Value dan fokus
-                    pada kebutuhan konsumen</p>
-            </div><!-- /.col-lg-3 -->
+                <p>Kami berorientasi pada Inovasi untuk meningkatkan Value dan fokus pada kebutuhan konsumen.</p>
+            </div>
 
             <!-- Implementation -->
-            <div class="col-lg-3">
-                <div class="container">
-                    <img src="{{ asset('images/implementation.png') }}" alt="Gambar Anda" width="150" height="130" style="margin-right: 20px; margin-left: 20px;">
-                </div>
-
-                <div class="transparent-container" style="background-color: rgba(255, 255, 255, 0.5); padding: 10px;"></div>
+            <div class="col-md-6 col-lg-3">
+                <img src="{{ asset('images/implementation.png') }}" alt="Implementation" width="150" height="130" class="mx-auto d-block mb-3">
                 <h2>Implementation</h2>
-                <p>Kami juga berorientasi pada
-                    Implementasi untuk merealisasikan
-                    ide dan inovasi secara nyata</p>
-            </div><!-- /.col-lg-3 -->
+                <p>Kami juga berorientasi pada Implementasi untuk merealisasikan ide dan inovasi secara nyata.</p>
+            </div>
 
             <!-- Transformation -->
-            <div class="col-lg-3">
-                <div class="container">
-                    <img src="{{ asset('images/transform.png') }}" alt="Gambar Anda" width="150" height="130" style="margin-right: 20px; margin-left: 20px;">
-                </div>
-
-                <div class="transparent-container" style="background-color: rgba(255, 255, 255, 0.5); padding: 10px;"></div>
+            <div class="col-md-6 col-lg-3">
+                <img src="{{ asset('images/transform.png') }}" alt="Transformation" width="150" height="130" class="mx-auto d-block mb-3">
                 <h2>Transformation</h2>
-                <p>Fokus utama kami pada perubahan
-                    kualitas personal dan profesional
-                    untuk meningkatkan produktifitas
-                    dalam mencapai prestasi kerja</p>
-            </div><!-- /.col-lg-3 -->
-        </div><!-- /.row -->
-
-
+                <p>Fokus utama kami pada perubahan kualitas personal dan profesional untuk meningkatkan produktivitas dalam mencapai prestasi kerja.</p>
+            </div>
+        </div>
     </div>
 </main>
--- resources/views/home/layouts/head.blade.php
+++ resources/views/home/layouts/head.blade.php
@@ -8,7 +8,7 @@
   <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
   <meta name="generator" content="Hugo 0.84.0">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sticky Footer Navbar Template · Bootstrap v5.0</title>
+  <title>PT Karunia Cipta Qinsani</title>
 
   <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
 
